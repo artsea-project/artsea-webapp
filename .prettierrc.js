4611c07a27840063f98a8129c46346556df353d2
Marta Kociszewska <martakociszewska04@gmail.com>
2026-05-19 23:21:16 +0200

    build: add Prettier configuration for code formatting

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+export default {
+  // Do not add semicolons at the end of statements
+  semi: false,
+
+  // Use double quotes instead of single quotes
+  singleQuote: false,
+
+  // Use 4 spaces for indentation
+  tabWidth: 4,
+
+  // Add trailing commas where valid in ES5 (objects, arrays, etc.)
+  trailingComma: "es5",
+
+  // Maximum line length before wrapping (100 characters)
+  printWidth: 100,
+
+  // Include parentheses around arrow function parameters
+  arrowParens: "always",
+};
